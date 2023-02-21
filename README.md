2a52dbd525b44855bbda7dc29c504c850e80c54b
unknown <lakshgaidhane20@gmail.com>
2023-02-21 10:34:51 +0530

    structure folder

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sensor-fault-detection
-sensor fault detection
+The air pressure system (APS) is a critical component of a heavy - duty vehicle that uses compressed air to force piston to provide pressure to the air brake pads , slowing vehicle down.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup (
+    name = "sensor",
+    version = "0.0.1",
+    author = "laksh",
+    author_email = "lakshgaidhane20@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+    
+)
